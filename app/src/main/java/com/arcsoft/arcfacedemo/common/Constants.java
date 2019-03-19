679eb38c17b599764135a83eb96d72ba7dfeac54
revstar <1967919189@qq.com>
2019-03-20 00:27:22 +0800

    虹软人脸检测

---
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -0,0 +1,6 @@
+package com.arcsoft.arcfacedemo.common;
+
+public class Constants {
+    public static final String APP_ID = "ChRsF7o5CvJbwU9WUfikMAxKZFZpravPtA6AZEhtS2tn";
+    public static final String SDK_KEY = "8qkKTnUp81sMAvdogukkvHLNFhZ5NjGgmMcYjn9KrWv8";
+}
